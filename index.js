3ad6aeffa073afc8b4ad7af521ad5ed3fd945120
Badmus damola <damolabadmus014@gmail.com>
2021-03-14 12:27:53 +0100

    adjust content

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-let open = document.querySelector('.nav-open');
-let close = document.querySelector('.nav-close');
-let nav = document.querySelector('.nav');
+let open = document.querySelector(".nav-open");
+let close = document.querySelector(".nav-close");
+let nav = document.querySelector(".nav");
+let menu = document.getElementsByTagName("a");
+console.log(menu);
 
+open.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
 
-open.addEventListener('click', ()=>{
-    nav.classList.add('visible')
-})
-close.addEventListener('click', ()=>{
-    nav.classList.remove('visible')
-})
+for (let i = 0; i < menu.length; i++) {
+  menu[i].addEventListener("click", () => {
+    nav.classList.remove("visible");
+  });
+}
+close.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
