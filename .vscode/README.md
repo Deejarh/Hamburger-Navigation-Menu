01c91a6501945798673ee5905e35ed0d02581de3
Badmus damola <damolabadmus014@gmail.com>
2021-03-14 09:30:00 +0100

    add Eventlistener

---
+++ .vscode/README.md
@@ -0,0 +1,5 @@
+# Navigation Menu (Hamburger)
+
+A simple Hamburger used for navigating content, using a mix of Event Listener and CSS properties.
+
+A simple Demo is available at 
--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let close = document.querySelector('.nav-close');
 let nav = document.querySelector('.nav');
 
 
+open.addEventListener('click', ()=>{
+    nav.classList.add('visible')
+})
+close.addEventListener('click', ()=>{
+    nav.classList.remove('visible')
+})
